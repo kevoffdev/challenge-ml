407613f2d352f3a250edf59cf9575568591ffd81
kevoff <suviacruzfer@gmail.com>
2024-02-18 18:14:57 -0300

    deleting unused files

--- src/ui/data.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Data({id}: {id: string}) {
-  const data = await fetch(`https://api.mercadolibre.com/categories/${id}`).then((resp) =>
-    resp.json(),
-  );
-
-  console.log(data);
-
-  return;
-}
